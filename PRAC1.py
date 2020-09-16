9a5744099ab0ec467cfe57a100538f1bb917bac3
ronak-parmar <ronakparmar133@gmail.com>
2020-09-16 15:01:26 +0530

    update

--- PRAC1.py
+++ PRAC1.py
@@ -1,1 +1,17 @@
-print("asdas")
+import telepot
+import datetime
+
+token='1216052230:AAG8mIKiPvma0Cqv2B4lRGEvSd13--3YAR8'
+bot = telepot.Bot(token)
+print (bot.getMe())
+
+    
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
+    if content_type == 'text':
+        if msg['text'].upper()=="DATE":
+            bot.sendMessage(chat_id,str(datetime.datetime.today().strftime("%x")))
+        else:
+            bot.sendMessage(chat_id,"Commend for Date is:-Date")
+bot.message_loop(handle)
